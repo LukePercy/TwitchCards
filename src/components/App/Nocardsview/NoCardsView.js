771326614f17648685d7016526ff104a5239c0ca
Luke Percy <lpercyagile@gmail.com>
2021-09-06 17:41:47 +1200

    WIP view for if the user has no cards in collection

---
+++ src/components/App/Nocardsview/NoCardsView.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NoCardsView.css";
+
+export default  function nocardsview(params) {
+    return (
+        <div>
+            <h1>DiceyDecks</h1>
+        </div>
+
+    );
+}

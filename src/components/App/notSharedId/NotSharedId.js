8a8950b4b8f892335b6e4eeb19485423c36426fd
Luke Percy <lpercyagile@gmail.com>
2021-09-29 23:12:16 +1300

    added new screen assets notsharedid

--- src/components/App/notSharedId/NotSharedId.js
+++ src/components/App/notSharedId/NotSharedId.js
@@ -3,11 +3,7 @@ import React from 'react';
 export default function NotSharedIdScreen() {
 
     return (
-        <div className="velvet">
-            <div className="title">
-            <h1>GettingDicey<span className="subtitle">Trading Cards</span></h1>
-            </div>
-            <div className="allowtext">Allow permissions below to start collecting!</div>
+        <div className='splash'>
         </div>
     )
 }

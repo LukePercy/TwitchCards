a9dd35fe36043f3846c124c7be46a8ae1da722f3
Luke Percy <lpercyagile@gmail.com>
2021-10-24 12:15:29 +1300

    adjusted card ids to match card number

--- src/components/App/cardList/CardList.js
+++ src/components/App/cardList/CardList.js
@@ -1,50 +1,50 @@
 import CardBack from '../cards/Card_Back-s1_worn.jpg';
 
 export const slides = [
+    {
+     id: 1,
+     title: 'DM',
+     subtitle: "The DM",
+     description: "It's Get...Getting Dicey!",
+     rarity: '',
+     frontimage: '',
+     backimage: CardBack
+    },
+    {
+     id: 2,
+     title: 'Bill',
+     subtitle: "Bard",
+     description: "...and my Lute!",
+     rarity: '',
+     frontimage: '',
+     backimage: CardBack
+    },
+    {
+     id: 3,
+     title: 'Byrem',
+     subtitle: "Wizard Necromancer",
+     description: "Leave no body behind",
+     rarity: '',
+     frontimage: '',
+     backimage: CardBack
 
-    {
-    id: 1, 
-    title: 'Miraqen',
-    subtitle: "The Druid",
-    description: "Adventure is never far away",
-    rarity: '',
-    frontimage: '',
-    backimage: CardBack
     },
     {
-    id: 2,
-    title: 'Byrem',
-    subtitle: "Wizard Necromancer",
-    description: "Leave no body behind",
-    rarity: '',
-    frontimage: '',
-    backimage: CardBack
+     id: 4, 
+     title: 'Miraqen',
+     subtitle: "The Druid",
+     description: "Adventure is never far away",
+     rarity: '',
+     frontimage: '',
+     backimage: CardBack
     },
     {
-    id: 3,
-    title: 'Morely',
-    subtitle: "Rogue",
-    description: "Run away!",
-    rarity: '',
-    frontimage: '',
-    backimage: CardBack
-    },
-    {
-    id: 4,
-    title: 'Bill',
-    subtitle: "Bard",
-    description: "...and my Lute!",
-    rarity: '',
-    frontimage: '',
-    backimage: CardBack
-    },
-    {
-    id: 5,
-    title: 'DM',
-    subtitle: "The DM",
-    description: "It's Get...Getting Dicey!",
-    rarity: '',
-    frontimage: '',
-    backimage: CardBack
+     id: 5,
+     title: 'Morely',
+     subtitle: "Rogue",
+     description: "Run away!",
+     rarity: '',
+     frontimage: '',
+     backimage: CardBack
     }
 ]

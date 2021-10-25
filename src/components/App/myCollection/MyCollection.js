cf741a36548643f4e28c095890f2d54e2f477bc1
Frank Liang <liangfeng610@gmail.com>
2021-10-25 13:20:56 +1300

    added channel auth context

--- src/components/App/myCollection/MyCollection.js
+++ src/components/App/myCollection/MyCollection.js
@@ -1,64 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 // Card info
-import { slides } from '../cardList/CardList';
-import useViewersCards from '../customHooks/useViewersCards';
+import useCardsForDisplay from '../customHooks/useCardsForDisplay';
 import ShowCardsImage from './ShowCardsImage/ShowCardsImage';
 import Loader from 'react-loader-spinner';
+import { ChannelAuthContext } from '../ChannelAuthContext';
+import useRedemption from '../customHooks/useRedemption';
 
-// const BASE_URL = 'http://localhost:3003/api/viewers';
+// const BASE_URL = 'http://localhost:3003';
 // const ORIGIN_URL = 'http://localhost:8080/';
 const BASE_URL = 'https://diceydeckbackend.herokuapp.com';
 const ORIGIN_URL = 'https://42xd9tib4hce93bavmhmseapyp7fwj.ext-twitch.tv';
 
 const initialState = {
   slideIndex: 0,
-};
-
-const useCardsForDisplay = (viewerId, channelId, twitchAuth) => {
-  let viewersCards;
-  if (!twitchAuth) {
-    viewersCards = useViewersCards(viewerId, channelId, twitchAuth);
-  }
-  const cardsForDisplay = viewersCards.map((holdingCard) => {
-    // use find() to compare two card IDs
-    // then return the matched card object
-    const matchedCard = slides.find((card) => holdingCard.cardId === card.id);
-    // Then update the card type for displaying
-    if (holdingCard.cardId === matchedCard.id) {
-      if (holdingCard.holdingAmount >= 0 && holdingCard.holdingAmount <= 5) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Worn', // we dont use this yet. Its used to drive CardRarity() display component.
-          frontimage: require(`../cards/${matchedCard.title}-s1_worn.jpg`),
-        };
-        return newCard;
-      } else if (
-        holdingCard.holdingAmount > 5 &&
-        holdingCard.holdingAmount <= 15
-      ) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Mint',
-          frontimage: require(`../cards/${matchedCard.title}-s1_mint.jpg`), //Mint card image
-          backimage: require(`../cards/Card_Back-s1_mint.jpg`),
-        };
-        return newCard;
-      } else if (holdingCard.holdingAmount > 15) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Foil',
-          frontimage: require(`../cards/${matchedCard.title}-s1_foil.jpg`), //Foil card image
-          backimage: require(`../cards/Card_Back-s1_foil.jpg`),
-        };
-        return newCard;
-      } else {
-        throw new Error(
-          'Should not happen. Holding amount out of numbered ranges' // if our ranges set fail, throw error
-        );
-      }
-    }
-  });
-  return cardsForDisplay;
 };
 
 // Slide navigation to view collection of cards
@@ -83,26 +37,16 @@ const slidesReducer = (state, event) => {
 };
 
 // Render cards in slide - see carousel.css
-export default function MyCollection({ viewerId, channelId }) {
-  const [state, dispatch] = React.useReducer(slidesReducer, initialState);
+const MyCollection = ({ viewerId, channelId }) => {
+  const twitchAuth = useContext(ChannelAuthContext);
+  const [state, dispatch] = useReducer(slidesReducer, initialState);
   const [hasViewerExisted, setViewerExisted] = useState(false);
   const [isLoading, setLoading] = useState(true);
-  const [twitchAuth, settwitchAuth] = useState(null);
-  let cardsForDisplay = useCardsForDisplay(viewerId, channelId, twitchAuth);
+  const cardsForDisplay = useCardsForDisplay(viewerId, channelId);
 
-  const getOAuth = async () => {
-    const response = await fetch(`${BASE_URL}/api/authinfo`);
-    const result = await response.json();
-    const { success, data } = result;
-    console.log(`data`, data);
-    if (success) {
-      settwitchAuth(data);
-    }
-  };
-
-  useEffect(() => {
-    getOAuth();
-  }, []);
+  // check whether uses redeem points from a top level.
+  const isRewardRedeemed = useRedemption(channelId, twitchAuth);
+  console.log('isRewardRedeemed :>> ', isRewardRedeemed);
 
   // use useEffect to fetch from DB check the viewer has existed in our DB
   useEffect(() => {
@@ -130,8 +74,9 @@ export default function MyCollection({ viewerId, channelId }) {
       setLoading(false);
     };
     getCardsViewer();
-  }, []);
+  }, [isRewardRedeemed]);
 
+  // Check the ternary expression
   return (
     <div className='slides'>
       {isLoading ? (
@@ -144,9 +89,10 @@ export default function MyCollection({ viewerId, channelId }) {
           cardsForDisplay={cardsForDisplay}
           viewerId={viewerId}
           channelId={channelId}
-          twitchAuth={twitchAuth}
         />
       )}
     </div>
   );
-}
+};
+
+export default MyCollection;

17ba975ab276d674faed91fa8d836787e1a12f80
Luke Percy <lpercyagile@gmail.com>
2021-10-26 20:21:12 +1300

    fixed merge issue

--- src/components/App/myCollection/MyCollection.js
+++ src/components/App/myCollection/MyCollection.js
@@ -15,56 +15,6 @@ const initialState = {
   slideIndex: 0,
 };
 
-<<<<<<< HEAD
-const useCardsForDisplay = (viewerId, channelId,twitchAuth) => {
-  let viewersCards;
-  if (!twitchAuth) {
-    viewersCards = useViewersCards(viewerId, channelId, twitchAuth);
-  }
-  const cardsForDisplay = viewersCards.map((holdingCard) => {
-    // use find() to compare two card IDs
-    // then return the matched card object
-    const matchedCard = slides.find((card) => holdingCard.cardId === card.id);
-    // Then update the card type for displaying
-    if (holdingCard.cardId === matchedCard.id) {
-      if (holdingCard.holdingAmount >= 0 && holdingCard.holdingAmount <= 5) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Worn', // we dont use this yet. Its used to drive CardRarity() display component.
-          frontimage: require(`../cards/${matchedCard.title}-s1_worn.jpg`),
-        };
-        return newCard;
-      } else if (
-        holdingCard.holdingAmount > 5 &&
-        holdingCard.holdingAmount <= 15
-      ) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Mint',
-          frontimage: require(`../cards/${matchedCard.title}-s1_mint.jpg`), //Mint card image
-          backimage: require(`../cards/Card_Back-s1_mint.jpg`),
-        };
-        return newCard;
-      } else if (holdingCard.holdingAmount > 15) {
-        const newCard = {
-          ...matchedCard,
-          rarity: 'Foil',
-          frontimage: require(`../cards/${matchedCard.title}-s1_foil.jpg`), //Foil card image
-          backimage: require(`../cards/Card_Back-s1_foil.jpg`),
-        };
-        return newCard;
-      } else {
-        throw new Error(
-          'Should not happen. Holding amount out of numbered ranges' // if our ranges set fail, throw error
-        );
-      }
-    }
-  });
-  return cardsForDisplay;
-};
-
-=======
->>>>>>> 4bc8f16cd227598e2b6805dc6126190b327d94a6
 // Slide navigation to view collection of cards
 const slidesReducer = (state, event) => {
   const { type, cardsForDisplay } = event;
